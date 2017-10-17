0a05236b1d8bd6220dbfaeb688e3c267705f84a7
shuai_global <shuai_global@atguigu.com>
2017-10-17 23:31:25 +0800

    泛型

---
+++ src/main/java/com/atguigu/gittest/Student.java
@@ -0,0 +1,42 @@
+/******************************************************************
+ *
+ *    Java Lib For Android, Powered By Shenzhen Jiuzhou.
+ *
+ *    Copyright (c) 2001-2014 Digital Telemedia Co.,Ltd
+ *    http://www.d-telemedia.com/
+ *
+ *    Package:     com.atguigu.gittest
+ *
+ *    Filename:    Student.java
+ *
+ *    Description: TODO(用一句话描述该文件做什么)
+ *
+ *    Copyright:   Copyright (c) 2001-2014
+ *
+ *    Company:     Digital Telemedia Co.,Ltd
+ *
+ *    @author:     Administrator
+ *
+ *    @version:    1.0.0
+ *
+ *    Create at:   2017年10月17日 下午9:11:23
+ *
+ *    Revision:
+ *
+ *    2017年10月17日 下午9:11:23
+ *        - first revision
+ *
+ *****************************************************************/
+package com.atguigu.gittest;
+
+
+/**
+ * @ClassName Student
+ * @Description TODO(这里用一句话描述这个类的作用)
+ * @author Administrator
+ * @Date 2017年10月17日 下午9:11:23
+ * @version 1.0.0
+ */
+public class Student {
+
+}
